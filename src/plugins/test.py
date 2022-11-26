4f15d41a828052c69fc40dc5602c26ed9b5a1671
mrhblfx <116722876+mrhblfx@users.noreply.github.com>
2022-11-26 16:31:06 +0800

    Update test.py

--- src/plugins/test.py
+++ src/plugins/test.py
@@ -1,5 +1,7 @@
-from fnbot import Send
+impor time
+
 from fnbot import Rev
+from fnbot import Send
 from fnbot import IstMsg
 
 @IstMsg.manage()
@@ -7,6 +9,19 @@ from fnbot import IstMsg
 async def _(msg_type:str, num_type:str, rev:'Rev'):
     if rev.match(['ciallo', 'こんにちは', '你好']):
         msg = 'ciallo!'
-        Send(rev).send_msg(msg_type,num_type,msg)
+        Send(rev).send_msg(msg_type, num_type, msg)
+        
+        
+@IstMsg.manage()
+@Rev.grace('/basic_time')
+async def _(msg_type:str, num_type:str, rev:'Rev'):
+    if rev.match('时间'):
+        wl = "一二三四五六天"
+        weekday = wl[time.localtime().tm_wday]
+        now_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        msg=(
+            f"{now_time}\t星期{weekday}"
+        )
+        Send(rev).send_msg(msg_type, num_type, msg)
 
 
