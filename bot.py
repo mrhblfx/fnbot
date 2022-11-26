bfac4aff85de0efd72756e9a4cccaaaaf6a0ac24
mrhblfx <116722876+mrhblfx@users.noreply.github.com>
2022-11-26 16:22:27 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,8 @@
+import fnbot
+
+fnbot.insert_plugin("test")
+
+if __name__ == "__main__":
+    fnbot.run()
+
+
---
+++ src/plugins/test.py
@@ -0,0 +1,12 @@
+from fnbot import Send
+from fnbot import Rev
+from fnbot import IstMsg
+
+@IstMsg.manage()
+@Rev.grace('/test')
+async def _(msg_type:str, num_type:str, rev:'Rev'):
+    if rev.match(['ciallo', 'こんにちは', '你好']):
+        msg = 'ciallo!'
+        Send(rev).send_msg(msg_type,num_type,msg)
+
+
