abbd7fd18e6d0bd8342c40c604655197b2cf65b5
mrhblfx <116722876+mrhblfx@users.noreply.github.com>
2022-11-28 19:00:25 +0800

    Update test.py

--- src/plugins/test.py
+++ src/plugins/test.py
@@ -1,15 +1,10 @@
-impor time
-
 from fnbot import Rev
 from fnbot import Send
 from fnbot import IstMsg
 
 @IstMsg.manage()
-@Rev.grace('/test')
+@Rev.grace()
 async def _(msg_type:str, num_type:str, rev:'Rev'):
-    if rev.match(['ciallo', 'こんにちは', '你好']):
+    if rev.match(['ciallo',]):
         msg = 'ciallo!'
         Send(rev).send_msg(msg_type, num_type, msg)
-        
-        
-      
